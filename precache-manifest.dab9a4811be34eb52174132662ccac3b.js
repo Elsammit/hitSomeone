8618eafb85469275cba70b83f6f762a5a976707c
Elsammit <hhashimo25@gmail.com>
2020-08-08 20:46:04 +0900

    Web app Release 3

---
+++ precache-manifest.dab9a4811be34eb52174132662ccac3b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6fdd46cd42e1d19d0046e89024d38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75fab3a46b5db1fc7fb9",
+    "url": "./static/css/main.7edc6050.chunk.css"
+  },
+  {
+    "revision": "dc4cd53a776b7df82c5f",
+    "url": "./static/js/2.6a949da5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6a949da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75fab3a46b5db1fc7fb9",
+    "url": "./static/js/main.59aa8764.chunk.js"
+  },
+  {
+    "revision": "6642e429f35f6f971cdf",
+    "url": "./static/js/runtime-main.20375881.js"
+  },
+  {
+    "revision": "df69fdf115c7671542425a7a499e8c1d",
+    "url": "./static/media/cave_Left.df69fdf1.png"
+  },
+  {
+    "revision": "3f9c141577ab7ff7268a3d1b8cfca15e",
+    "url": "./static/media/cave_Right.3f9c1415.png"
+  },
+  {
+    "revision": "238737090cbb426b5ebae4ea0164a6e1",
+    "url": "./static/media/inu.23873709.png"
+  },
+  {
+    "revision": "2d17e91f5383c8c582aa7b90e7b58460",
+    "url": "./static/media/mogura.2d17e91f.png"
+  },
+  {
+    "revision": "58f37357020b63563e2c9deab601afc6",
+    "url": "./static/media/saru.58f37357.png"
+  },
+  {
+    "revision": "ead25118b043114ba9797e9a001d9768",
+    "url": "./static/media/wani.ead25118.png"
+  }
+]);
