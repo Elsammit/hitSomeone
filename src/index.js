77fb1f3c329a54ca827ff2e9c1ad4994adf9f52c
Elsammit <hhashimo25@gmail.com>
2020-08-08 20:30:20 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Waniwani from './waniwani';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Waniwani />
   </React.StrictMode>,
   document.getElementById('root')
 );
